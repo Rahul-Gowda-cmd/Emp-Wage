84e071184fd1320a852b2cf1cb2758b2933a4b81
Rahul prabhu <rahul.prabu.07@gmail.com>
2021-07-23 18:25:16 +0530

    Added: EmployeeAttendence

--- Emp-Wage.sh
+++ Emp-Wage.sh
@@ -1,1 +1,11 @@
-#welcome to the program
+#!/bin/bash -x
+
+empCheck=$((RANDOM%2))
+isPresent=1
+
+if [ $empCheck -eq $isPresent ]
+then
+      echo Employee is present
+else
+      echo Employee is present
+fi
