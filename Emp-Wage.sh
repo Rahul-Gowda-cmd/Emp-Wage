931736b76ee5a1b7c3ce6f02474e8f00ae0e0d27
Rahul prabhu <rahul.prabu.07@gmail.com>
2021-07-24 12:58:33 +0530

    Added:Monthly Wage added

--- Emp-Wage.sh
+++ Emp-Wage.sh
@@ -1,19 +1,29 @@
 #!/bin/bash -x
 
-isFullTime=1
-isPartTime=2
-empRatePerHr=20
+#constant
+IS_FULL_TIME=1
+IS_PART_TIME=2
+EMP_RATE_PER_HR=20
+NUM_OF_WORKING_DAYS=20
 
-empCheck=$((RANDOM%3))
-case $empCheck in 
-	$isFullTime) 
-	 empHrs=8
-        ;;
-	$isPartTime) 
-	 empHrs=4
-	;;
-	*)
-	 empHrs=0
-	;;
-esac
-wage=$(($empRatePerHr*$empHrs))
+#variable
+totalsalary=0
+
+for(( day=1; day<=$NUM_OF_WORKING_DAYS; day++ ))
+do
+   empCheck=$((RANDOM%3))
+   case $empCheck in
+     $IS_FULL_TIME)
+        empHrs=8
+     ;;
+    $IS_PART_TIME)
+       empHrs=4
+     ;;
+     *)
+       empHrs=0
+     ;;
+   esac
+wage=$(($EMP_RATE_PER_HR*$empHrs))
+totalsalary=$(($totalsalary+$wage))
+done
+
